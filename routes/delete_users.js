55d240102ac4951bf286b4a909dc5f2e606b673d
Adham-Mohamed7 <150583508+Adham-Mohamed7@users.noreply.github.com>
2025-05-08 17:57:24 +0300

    Update delete_users.js

--- routes/delete_users.js
+++ routes/delete_users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { deleteUser } = require('../controllers/users'); 
 
 
-router.delete('users/:id', deleteUser);
+router.delete('/:id', deleteUser);
 
 
 module.exports = router;
